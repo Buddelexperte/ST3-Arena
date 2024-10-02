c69640f281e40c1ab073c393188a84c79575f3d3
Buddelexperte <74837601+Buddelexperte@users.noreply.github.com>
2024-10-02 11:44:56 +0200

    Improved framework - General drawShape function - dynamic Vector for shapes

--- SFML_Playground/main.cpp
+++ SFML_Playground/main.cpp
@@ -1,10 +1,16 @@
 #include <SFML/Graphics.hpp>
+#include <vector>
+
+void drawShapes(sf::RenderWindow&, const std::vector<sf::Drawable*>&);
 
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
-    sf::CircleShape shape(100.f);
-    shape.setFillColor(sf::Color::Green);
+    sf::RenderWindow window(sf::VideoMode(1920, 1080), "SFML_Clicker", sf::Style::Fullscreen);
+
+    // Vector for shape storage and access
+    std::vector<sf::Drawable*> shapes;
+
+    // Creation of Shapes
 
     while (window.isOpen())
     {
@@ -14,11 +20,24 @@ int main()
             if (event.type == sf::Event::Closed)
                 window.close();
         }
-
+        
         window.clear();
-        window.draw(shape);
+        // Draw Shapes
+        drawShapes(window, shapes);
+        // Display Draw changes
         window.display();
     }
 
+    // Clean up dynamically allocated shapes
+    for (auto shape : shapes) {
+        delete shape;
+    }
+
     return 0;
+}
+
+void drawShapes(sf::RenderWindow& window, const std::vector<sf::Drawable*>& shapes) {
+    for (const auto& shape : shapes) {
+        window.draw(*shape);
+    }
 }
