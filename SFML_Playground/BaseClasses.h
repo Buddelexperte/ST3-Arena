509024811a81df4a896976b3c7b8f8f65dc7e8e7
Buddelexperte <74837601+Buddelexperte@users.noreply.github.com>
2025-02-19 19:23:08 +0100

    Changed Filters for organization

--- SFML_Playground/BaseClasses.h
+++ SFML_Playground/BaseClasses.h
@@ -1,6 +1,7 @@
 #pragma once
 #include "Button.h"
 #include "BaseTypes.h"
+#include "Player.h"
 #include "Collision.h"
 
 class GI_Arena;
@@ -86,51 +87,6 @@ public:
 	virtual bool handleInput(sf::Event* eventRef);
 	virtual bool isMouseOver(const bool& = false) { return false; }
 };
-
-
-// PLAYER -----------------------------------------------------------------------------------------
-
-class Player : public InputWidget, public ICollidable
-{
-private:
-	sf::Sprite playerSprite;
-
-	CollisionBox collisionBox;
-
-	RenderInfo renderInfo;
-	sf::Vector2f direction = { 0.0f, 0.0f };
-
-	std::vector<sf::Texture> playerTextures = {};
-	int currentFrame = 0;
-	float animationAccu = 0.0f;
-	float animationSpeed = 0.0f;
-
-	void tick_collision(const float& deltaTime) override;
-	void calcMovement(const float&);
-protected:
-	sf::Keyboard::Key keyboardInput(sf::Event*) override;
-	sf::Mouse::Button mouseInput(sf::Event*) override;
-	float scrollInput(sf::Event*) override;
-public:
-	Player(InputWidget*);
-	void tick(const float&) override;
-	void setPos(const sf::Vector2f&) override;
-	void addPos(const sf::Vector2f&) override;
-	sf::Vector2f getPos() const override;
-	void setRot(const float&) override;
-	float getRot() const override;
-	void setSize(const sf::Vector2f&) override;
-	sf::Vector2f getVelocity() const { return renderInfo.velocity; };
-	sf::Vector2f getDirection() const { return direction; };
-
-	RenderInfo getRenderInfo() const { return renderInfo; }
-	// Collision-Interface
-	sf::FloatRect getCollisionBounds() const override { return collisionBox.getCollisionBounds(); }
-	bool isColliding(const sf::FloatRect& otherBox) const override;
-	bool isColliding(const sf::Vector2f& otherPos) const override;
-	void onCollision(ICollidable* other) override;
-};
-
 
 // SAVE GAME --------------------------------------------------------------------------------------
 
